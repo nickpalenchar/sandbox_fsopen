8c414b45be4526bc9b702353aeeb6d4f00f2673c
Nick Palenchar <nickpal@nickpalenchar.com>
2026-01-19 17:25:55 -0500

    exercises 1.3

--- exercises/1.1_1.2/src/App.jsx
+++ exercises/1.1_1.2/src/App.jsx
@@ -1,25 +1,33 @@
-const Part = ({ page, title }) => {
+const Part = ({ content }) => {
+  const { name, exercises } = content;
   return <>
-    <p>{title} {page}</p>
+    <p>{name} {exercises}</p>
   </>
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <h1>{course}</h1>
-      <Part page={exercises1} title={part1} />
-      <Part page={exercises2} title={part2} />
-      <Part page={exercises3} title={part3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Part content={part1} />
+      <Part content={part2} />
+      <Part content={part3} />
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
